cf1f4d3e06c89e6699211407fe996456acdd7ec0
nianfouyi <876217348@qq.com>
2020-08-21 17:31:27 +0800

    树练习

--- souptest.py
+++ souptest.py
@@ -15,40 +15,45 @@ headers = {
         }
 
 
-async def get(url):
+async def get(sem, url_):
     # headless参数设为False，则变成有头模式
-    browser = await launch(
+    async with sem:
+        browser = await launch(
         # headless=False
-    )
+        )
+        page = await browser.newPage()
+        # 设置页面视图大小
+        await page.setViewport(viewport={'width': 1280, 'height': 800})
+        # 是否启用JS，enabled设为False，则无渲染效果
+        await page.setJavaScriptEnabled(enabled=True)
+        await page.goto(url_)
+        try:
+            soup = BeautifulSoup(await page.content(), 'lxml')
+        # print(soup.tbody)
+            print(type(soup))
+            infos = soup.tbody.find_all('td')
 
-    page = await browser.newPage()
-
-    # 设置页面视图大小
-    await page.setViewport(viewport={'width': 1280, 'height': 800})
-
-    # 是否启用JS，enabled设为False，则无渲染效果
-    await page.setJavaScriptEnabled(enabled=True)
-
-    await page.goto(url)
-
-    soup = BeautifulSoup(await page.content(), 'lxml')
-    # print(soup.tbody)
-    infos = soup.tbody.find_all('td')
-
-    infos = [infos[i:i+7] for i in range(0, len(infos)+1, 7)]
-    # print(infos)
-    for x in infos:
-        print((x[0].contents[0], x[1].contents[0], x[2].contents[0], x[3].contents[0], x[4].contents[0], x[5].contents[0], x[6].contents[0]))
+            infos = [infos[i:i+7] for i in range(0, len(infos)+1, 7)]
+        # print(infos)
+            for x in infos:
+                try:
+                    print((x[0].contents[0], x[1].contents[0], x[2].contents[0], x[3].contents[0], x[4].contents[0], x[5].contents[0], x[6].contents[0]))
+                except Exception as e:
+                    print(e)
+        except Exception as e:
+            print(e)
     # 关闭浏览器
-    await browser.close()
+        await browser.close()
 
 
 if __name__ == '__main__':
     tasks = []
-    for x in range(1, 5):
+    for x in range(1, 20):
         url = f'https://www.kuaidaili.com/free/inha/{x}/'
+        # 设置并发的数量
+        sem = asyncio.Semaphore(1)
         loop = asyncio.get_event_loop()
-        task = loop.create_task(get(url))
+        task = loop.create_task(get(sem, url))
         tasks.append(task)
     asyncio.get_event_loop().run_until_complete(asyncio.wait(tasks))
 
